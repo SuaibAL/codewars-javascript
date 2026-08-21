b9c8968565efdbb8c0b7cab2bffdd8553e64045c
Suaib Al Hasan <suaib.al.hasannn@gmail.com>
2026-08-22 00:00:51 +0800

    Update exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string.js

--- 8-kyu/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string.js
+++ 8-kyu/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string.js
@@ -1,3 +1,16 @@
+/*প্রথমে শেষ character বের করব।
+তারপর check করব সেটা "!" কিনা।
+যদি হয়, তাহলে শেষ character বাদ দেব।
+না হলে original string return করব।*/
+
+/*Concept:
+- const
+- string indexing
+- string.length
+- if/else
+- slice()*/
+
+
 function remove (string) {
   const lastcharacter = string[string.length - 1];
   if (lastcharacter==="!"){
